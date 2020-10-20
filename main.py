afe59b7f062246843f2d4df9d70c7c352adfeb58
kevin5221 <71689294+kevin5221@users.noreply.github.com>
2020-10-20 15:50:20 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,61 @@
+x = 0
+y = 0
+
 def on_forever():
-    for y in range(5):
-        for x in range(5):
-            led.plot(x, y)
-            basic.pause(500)
+    global x, y
+    x = 4
+    y = 0
+    while True:
+        led.plot(x, y)
+        basic.pause(200)
+        basic.clear_screen()
+        while True:
+            led.plot(3, 0)
+            led.plot(4, 1)
+            basic.pause(200)
+            basic.clear_screen()
+            while True:
+                led.plot(2, 0)
+                led.plot(3, 1)
+                led.plot(4, 2)
+                basic.pause(200)
+                basic.clear_screen()
+                while True:
+                    led.plot(1, 0)
+                    led.plot(2, 1)
+                    led.plot(3, 2)
+                    led.plot(4, 3)
+                    basic.pause(200)
+                    basic.clear_screen()
+                    while True:
+                        led.plot(0, 0)
+                        led.plot(1, 1)
+                        led.plot(2, 2)
+                        led.plot(3, 3)
+                        led.plot(4, 4)
+                        led.plot(5, 5)
+                        basic.pause(200)
+                        basic.clear_screen()
+                        while True:
+                            led.plot(0, 1)
+                            led.plot(1, 2)
+                            led.plot(2, 3)
+                            led.plot(3, 4)
+                            basic.pause(200)
+                            basic.clear_screen()
+                            while True:
+                                led.plot(0, 2)
+                                led.plot(1, 3)
+                                led.plot(2, 4)
+                                basic.pause(200)
+                                basic.clear_screen()
+                                while True:
+                                    led.plot(0, 3)
+                                    led.plot(1, 4)
+                                    basic.pause(200)
+                                    basic.clear_screen()
+                                    while True:
+                                        led.plot(0, 4)
+                                        basic.pause(200)
+                                        basic.clear_screen()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,8 +1,68 @@
+let x = 0
+let y = 0
 basic.forever(function () {
-    for (let y = 0; y <= 4; y++) {
-        for (let x = 0; x <= 4; x++) {
-            led.plot(x, y)
-            basic.pause(500)
+    x = 4
+    y = 0
+    while (true) {
+        led.plot(x, y)
+        basic.pause(200)
+        basic.clearScreen()
+        while (true) {
+            led.plot(3, 0)
+            led.plot(4, 1)
+            basic.pause(200)
+            basic.clearScreen()
+            while (true) {
+                led.plot(2, 0)
+                led.plot(3, 1)
+                led.plot(4, 2)
+                basic.pause(200)
+                basic.clearScreen()
+                while (true) {
+                    led.plot(1, 0)
+                    led.plot(2, 1)
+                    led.plot(3, 2)
+                    led.plot(4, 3)
+                    basic.pause(200)
+                    basic.clearScreen()
+                    while (true) {
+                        led.plot(0, 0)
+                        led.plot(1, 1)
+                        led.plot(2, 2)
+                        led.plot(3, 3)
+                        led.plot(4, 4)
+                        led.plot(5, 5)
+                        basic.pause(200)
+                        basic.clearScreen()
+                        while (true) {
+                            led.plot(0, 1)
+                            led.plot(1, 2)
+                            led.plot(2, 3)
+                            led.plot(3, 4)
+                            basic.pause(200)
+                            basic.clearScreen()
+                            while (true) {
+                                led.plot(0, 2)
+                                led.plot(1, 3)
+                                led.plot(2, 4)
+                                basic.pause(200)
+                                basic.clearScreen()
+                                while (true) {
+                                    led.plot(0, 3)
+                                    led.plot(1, 4)
+                                    basic.pause(200)
+                                    basic.clearScreen()
+                                    while (true) {
+                                        led.plot(0, 4)
+                                        basic.pause(200)
+                                        basic.clearScreen()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 })
